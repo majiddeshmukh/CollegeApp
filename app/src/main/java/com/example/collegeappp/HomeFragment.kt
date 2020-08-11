31a9e986cb4d3c8a83b0436ef402bfde9b2987ae
droidy12527 <gundiaramasu@gmail.com>
2020-08-11 22:00:33 +0530

    Fragment Linked to Activity

--- app/src/main/java/com/example/collegeappp/HomeFragment.kt
+++ app/src/main/java/com/example/collegeappp/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import kotlinx.android.synthetic.main.fragment_home.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,21 +26,32 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
 
 
+
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+         dfill.setOnClickListener {
+            //FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this@HomeFragment.context, DataActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
     }
 
     companion object {
